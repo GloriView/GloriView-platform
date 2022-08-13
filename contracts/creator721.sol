8d576eace52001c34a2f62c7c9b2cde3edd0bd0e
GloriView <102840415+GloriView@users.noreply.github.com>
2022-08-13 10:36:53 +0800

    Update creator721.sol

--- contracts/creator721.sol
+++ contracts/creator721.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../interface/INFT.sol";
+import "./interface/INFT.sol";
 import "erc721a/contracts/ERC721A.sol";
 
 
