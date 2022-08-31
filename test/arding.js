f046e1eecb6470ee4af8b540dbae350a67440257
GloriView <102840415+GloriView@users.noreply.github.com>
2022-08-31 17:30:57 +0800

    Update arding.js

--- test/arding.js
+++ test/arding.js
@@ -21,3 +21,4 @@ assert.isTrue(await forwarder.isForwarder(), 'should be a forwarder')
  it('reports correct forwarder type', async () => {
      assert.equal(await forwarder.forwarderType(), ForwarderType.NO_CONTEXT, 'should report correct forwarding type')
     })
+it('can forward', async () => {
