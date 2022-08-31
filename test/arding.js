8920fd3c33e137cd0fc25a478cae11b37441e66f
GloriView <102840415+GloriView@users.noreply.github.com>
2022-08-31 17:27:21 +0800

    Update arding.js

--- test/arding.js
+++ test/arding.js
@@ -1,2 +1,3 @@
 const { EMPTY_BYTES } = require('@aragon/contract-helpers-test')
 const { assertRevert } = require('@aragon/contract-helpers-test/src/asserts')
+const ForwarderMock = artifacts.require('ForwarderMock')
